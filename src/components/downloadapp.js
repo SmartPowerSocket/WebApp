7c9e022736b4d983c4eecbf4c7518abb4fcb7617
Daniel Parreira <daniel@Daniels-MacBook-Pro.local>
2016-07-15 00:52:28 -0300

    Deploy

--- src/components/downloadapp.js
+++ src/components/downloadapp.js
@@ -10,33 +10,33 @@ class DownloadApp extends Component {
                       <br />
                       <div className="panel panel-default">
                           <div className="panel-body" style={{'textAlign': 'center'}}>
-                            <a href="https://www.dropbox.com/s/aav5uf9gv08gvt5/smartpowersocket-app-win32-ia32.zip?dl=1">
+                            <a href="https://www.dropbox.com/s/7zrz0sv9e7wdnyb/win32-ia32.zip?dl=1">
                               <button type="button" className="btn btn-default button-marging">
-                                <img src="images/windows.png" width="100px" height="100px" alt="Windows 32 bits app"></img> 
+                                <img src="images/windows.png" width="100px" height="100px" alt="Windows 32 bits app"></img>
                                 <br />Windows 32 bits
                               </button>
                             </a>
-                            <a href="https://www.dropbox.com/s/kohkcc14yuoikcu/smartpowersocket-app-win32-x64.zip?dl=1">
+                            <a href="https://www.dropbox.com/s/xi054aykf04d4f9/win32-x64.zip?dl=1">
                               <button type="button" className="btn btn-default button-marging">
-                                <img src="images/windows.png" width="100px" height="100px" alt="Windows 64 bits app"></img> 
+                                <img src="images/windows.png" width="100px" height="100px" alt="Windows 64 bits app"></img>
                                 <br />Windows 64 bits
                               </button>
                             </a>
-                            <a href="https://www.dropbox.com/s/dchq8js2eaoxrp1/smartpowersocket-app-darwinx64.zip?dl=1">
+                            <a href="https://www.dropbox.com/s/53cppm0b3w6ieke/darwin-x64.zip?dl=1">
                               <button type="button" className="btn btn-default button-marging">
-                                <img src="images/apple.png" width="100px" height="100px" alt="Mac OSX app"></img> 
+                                <img src="images/apple.png" width="100px" height="100px" alt="Mac OSX app"></img>
                                 <br />Mac OSX
                               </button>
                             </a>
-                            <a href="https://www.dropbox.com/s/abmdpy60ilq7al7/smartpowersocket-app-linux-ia32.zip?dl=1">
+                            <a href="https://www.dropbox.com/s/no2tenhywb3vtsx/linux-ia32.zip?dl=1">
                               <button type="button" className="btn btn-default button-marging">
-                                <img src="images/linux.png" width="100px" height="100px" alt="Linux 32 bits app"></img> 
+                                <img src="images/linux.png" width="100px" height="100px" alt="Linux 32 bits app"></img>
                                 <br />Linux 32 bits
                               </button>
                             </a>
-                            <a href="https://www.dropbox.com/s/u4vgnl7i7fkoh94/smartpowersocket-app-linux-x64.zip?dl=1">
+                            <a href="https://www.dropbox.com/s/20dqtsz8eugy257/linux-x64.zip?dl=1">
                               <button type="button" className="btn btn-default">
-                                <img src="images/linux.png" width="100px" height="100px" alt="Linux 64 bits app"></img> 
+                                <img src="images/linux.png" width="100px" height="100px" alt="Linux 64 bits app"></img>
                                 <br />Linux 64 bits
                               </button>
                             </a>
